dfdbddca1438a9bbb0c8209c1b4f23c8f864817c
trupti101101 <truptipathade354@gmail.com>
2024-06-18 00:04:22 +0530

    TO DO APP

--- ToDoApp.py
+++ ToDoApp.py
@@ -21,15 +21,24 @@ class ToDoApp:
         else:
             print("Invalid task number.")
 
+    def update_task(self, task_number, new_task):
+        if 0 < task_number <= len(self.tasks):
+            old_task = self.tasks[task_number - 1]
+            self.tasks[task_number - 1] = new_task
+            print(f"Task '{old_task}' updated to '{new_task}'.")
+        else:
+            print("Invalid task number.")
+
     def run(self):
         while True:
             print("\n==| Welcome To 'TO DO App' |==")
             print("\nOptions:")
             print("1. Add Task")
             print("2. View Tasks")
-            print("3. Delete Task")
-            print("4. Exit")
-            choice = input("Enter your choice (1/2/3/4): ")
+            print("3. Delete Task") 
+            print("4. Update Task")
+            print("5. Exit")
+            choice = input("Enter your choice (1/2/3/4/5): ")
 
             if choice == '1':
                 task = input("Enter the task: ")
@@ -43,6 +52,13 @@ class ToDoApp:
                 except ValueError:
                     print("Please enter a valid number.")
             elif choice == '4':
+                try:
+                    task_number = int(input("Enter the task number to update: "))
+                    new_task = input("Enter the new task: ")
+                    self.update_task(task_number, new_task)
+                except ValueError:
+                    print("Please enter a valid number.")
+            elif choice == '5':
                 print("Exiting the To-Do app!")
                 break
             else:
